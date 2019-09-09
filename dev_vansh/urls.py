50fc08422d25d5ddd82f1d1fcdc5662c5b8e2914
vansh1999 <vansh.bhardwaj1999@gmail.com>
2019-09-10 03:42:10 +0530

    added home page

--- dev_vansh/urls.py
+++ dev_vansh/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("projects/" , include("projects.urls")),
     path("blog/" , include("blog.urls")),
+    path("" , include("homepage.urls")),
 ]
 
