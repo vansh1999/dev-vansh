bd694f8bdeb03ee3523cfb4e5d1d4fa51c63887a
vansh1999 <vansh.bhardwaj1999@gmail.com>
2019-11-21 23:13:10 +0530

    conatct form00

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("" , views.Index , name = "Index"),
+    path("contact/" , views.Contact , name = "contact")
 
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -6,3 +6,7 @@ from django.shortcuts import render
 def Index(request):
 
     return render(request , "index.html")
+
+
+def Contact(request):
+    return render(request , "contact-form.html")
